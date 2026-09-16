d8dc9263f89a9f8f28254f838e5778a82bc5ccdb
Ardiw123 <arvid.wetterlind@gmail.com>
2026-09-16 15:51:08 +0200

    Fix comit

--- Ball.cs
+++ Ball.cs
@@ -3,8 +3,7 @@ class Ball : GameObject, IMovable, ICollidable
     public Sprite sprite;
 
     float speed = 500;
-
-    const float radius = 10;
+    const float radius = 25;
     const float diamiter = radius * 2;
 
     public Ball(GameObjectHandler h) : base(h)
@@ -14,11 +13,13 @@ class Ball : GameObject, IMovable, ICollidable
         sprite.Position = new Vector2f(250, 300);
         pos = new(200, 200);
         size = new(200, 200);
+
+        sprite.Scale = new Vector2f(diamiter / sprite.Texture.Size.X, diamiter / sprite.Texture.Size.Y);
     }
 
     public override void Update(float deltaTime)
     {
-        
+
         Vector2f direction = new(500, 1);
 
         ((IMovable)this).MoveObject(ref pos, ((IMovable)this).CalculateMoveVector(direction, speed * deltaTime));
